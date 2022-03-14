6886746eb369b5ddc1e88ada716e8f08f5dbe8ea
ChefDimas <dimalunik@aol.com>
2022-03-14 19:49:21 +0200

    changes

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,19 @@
 export default function Navbar() {
   return (
-    <div className="navbar">
-      <a href="#!">
+    <div className="navbar container">
+      <a href="#!" className="logo">
         Mo'<span>Food</span>
       </a>
+      <div className="nav-links">
+        <a href="!#">Home</a>
+        <a href="!#">Recipes</a>
+        <a href="!#">Settings</a>
+      </div>
+      <div className="sidebar-btn active">
+        <div className="bar"></div>
+        <div className="bar"></div>
+        <div className="bar"></div>
+      </div>
     </div>
   );
 }
